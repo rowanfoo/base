9197e83a0742abb5be23dae3c1091e6b86525004
rowanfoo <rowanfoo@gmail.com>
2019-10-07 20:17:28 +0800

    first commit

---
+++ algodata/src/main/kotlin/com/dhamma/algodata/algodata/RSI.kt
@@ -0,0 +1,74 @@
+package com.dhamma.algodata.algodata
+
+import com.dhamma.algodata.utility.Calc
+import com.dhamma.base.ignite.IgniteRepo
+import com.dhamma.base.ignite.concurrency.IgniteCacheConcurency
+import com.dhamma.pesistence.entity.data.CoreData
+import com.google.gson.JsonObject
+import org.apache.ignite.Ignite
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+//import com.dhamma.concurrency
+
+@Component
+class RSI {
+    @Autowired
+    lateinit var stocklist: List<String>
+
+    @Autowired
+    lateinit var ignitecache: IgniteRepo<CoreData>
+
+    @Autowired
+    lateinit var ignite: Ignite
+
+    @Autowired
+    lateinit var cacheConcurency: IgniteCacheConcurency
+
+
+    fun process(a: JsonObject) {
+        //      var from: Int = a.get("from").asInt
+        //   var to: Int = a.get("to").asInt
+        var time: Int = a.get("time").asInt
+        var code: String = a.get("code").asString
+        println("--------WILL PROCESS ----------")
+
+
+        var cache = ignite.getOrCreateCache<String, Pair<Double, String>>("RSI$time")
+
+        var series = ignitecache.values(" where code=?  order by date desc  LIMIT ? ", arrayOf(code, "$time"))
+        println("--------load----SIZE-------------${series.size}")
+
+        var num = Calc().calculateRsi(series.reversed())
+        var max = series.maxBy { it.close }!!.close
+        var min = series.minBy { it.close }!!.close
+        var percent = String.format("%.1f", (((max - min) / max) * 100))
+
+        println("--------AVG--------$code------------$num")
+        cache.put("$code", Pair(String.format("%.1f", num).toDouble(), "$max - $min   $percent"))
+
+
+    }
+
+    private fun runload(obj: JsonObject) {
+        println("--------RUNLOAD   RSI----------")
+        stocklist.parallelStream().forEach {
+            var content = JsonObject()
+            content.addProperty("code", it)
+            content.addProperty("time", obj.get("rsi").asInt)
+            process(content)
+        }
+        println("--------DONE ALL  RSI----------")
+
+    }
+
+    fun loadall(obj: JsonObject) {
+        println("--------LOAD ALL  RSI----------")
+        var time = obj.get("rsi").asInt
+        cacheConcurency.process("RSI$time", obj, ::runload)
+
+
+    }
+
+
+}
---
+++ algodata/src/main/kotlin/com/dhamma/algodata/algodata/VolumeMA.kt
@@ -0,0 +1,101 @@
+package com.dhamma.algodata.algodata
+
+import com.dhamma.algodata.utility.Calc
+import com.dhamma.base.ignite.IgniteRepo
+import com.dhamma.base.ignite.concurrency.IgniteCacheConcurency
+import com.dhamma.pesistence.entity.data.CoreData
+import com.google.gson.JsonObject
+import org.apache.ignite.Ignite
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class VolumeMA {
+
+    @Autowired
+    lateinit var stocklist: List<String>
+    @Autowired
+    lateinit var ignite: Ignite
+    @Autowired
+    lateinit var ignitecache: IgniteRepo<CoreData>
+
+    @Autowired
+    lateinit var cacheConcurency: IgniteCacheConcurency
+
+
+    fun process(data: JsonObject) {
+        var volumema = data.get("volumema").asInt
+
+        println("-------VolumeMA------$volumema-----")
+
+        var cache = ignite.getOrCreateCache<String, Double>("MA$volumema:vol")
+
+        if (cache.size() == 0) {
+
+
+            stocklist.forEach {
+                // var series = ignitecache.get("$it")
+                println("--------load----CODE-------------$it")
+                var series = ignitecache.values(" where code=?  order by date desc  LIMIT ? ", arrayOf(it, "$volumema"))
+
+                println("--------load----SIZE-------------${series.size}")
+
+                var num = Calc().mocingaverage(volumema, series, "vol")
+
+                println("--------AVG--------$it------------$num")
+                cache.put("$it", num)
+
+            }
+
+
+        }
+
+
+    }
+
+
+    fun runload(obj: JsonObject) {
+        var volumema = obj.get("volumema").asInt
+
+        println("-------VolumeMA------$volumema-----")
+
+        var cache = ignite.getOrCreateCache<String, Double>("MA$volumema:vol")
+
+        if (cache.size() == 0) {
+
+
+            stocklist.parallelStream().forEach {
+                // var series = ignitecache.get("$it")
+                println("--------load----CODE-------------$it")
+                var series = ignitecache.values(" where code=?  order by date desc  LIMIT ? ", arrayOf(it, "$volumema"))
+
+                println("--------load----SIZE-------------${series.size}")
+
+                var num = Calc().mocingaverage(volumema, series, "vol")
+
+                println("--------AVG--------$it------------$num")
+                cache.put("$it", num)
+
+            }
+
+
+        }
+
+
+        println("--------DONE ALL  VOLUMEA----------")
+
+    }
+
+    fun loadall(obj: JsonObject) {
+        println("--------LOAD ALL  VOLUMEAVG----------")
+        var volumema = obj.get("volumema").asInt
+
+        println("-------VolumeMA------$volumema-----")
+
+        cacheConcurency.process("MA$volumema:vol", obj, ::runload)
+
+
+    }
+
+
+}
