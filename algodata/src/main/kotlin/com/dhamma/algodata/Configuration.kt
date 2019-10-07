d72a490771f304f410dae3aadb1ad5e0f2fe25e6
rowanfoo <rowanfoo@gmail.com>
2019-10-07 20:18:55 +0800

    first commit

---
+++ algodata/src/main/kotlin/com/dhamma/algodata/Configuration.kt
@@ -0,0 +1,20 @@
+package com.dhamma.algodata
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class Configuration {
+//    @Autowired
+//    lateinit var stockrepo: StockRepo
+
+//    @Bean
+//    fun stocklist(): List<String> {
+//        println("------------------------------STOCKLIST--------------")
+//        var list = stockrepo.findAll()
+//        return list.map { it.code }.toList()
+//    }
+//
+//
+
+
+}
