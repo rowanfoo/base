92e33220f880f35585d53b50e019d3181d31ca40
rowanfoo <rowanfoo@gmail.com>
2020-01-09 04:18:25 +0800

    refactor package

--- algodata/src/main/kotlin/com/dhamma/ignitedata/AlgoDataApplication.kt
+++ algodata/src/main/kotlin/com/dhamma/ignitedata/AlgoDataApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan(basePackages = ["com.dhamma.base.ignite", "com.dhamma.pesistence", "com.dhamma.ignitedao"])
+@ComponentScan(basePackages = ["com.dhamma.base.ignite", "com.dhamma.pesistence", "com.dhamma.ignitedata"])
 class AlgoDataApplication
 
 fun main(args: Array<String>) {
