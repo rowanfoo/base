8502fc5307f7baaeb58b36656463133324ac93d2
rowan <rowanfoo@gmail.com>
2020-01-08 11:29:29 +0800

    clean up and add comments

--- ignite/src/main/kotlin/com/dhamma/base/ignite/concurrency/IgniteCacheConcurency.kt
+++ ignite/src/main/kotlin/com/dhamma/base/ignite/concurrency/IgniteCacheConcurency.kt
@@ -8,114 +8,18 @@ import org.apache.ignite.configuration.CacheConfiguration
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
-
+/*
+Cannot just have a simple synhronize , because app might run multiple copy , therefore coredata cache could be populate double
+using distributed locks
+ */
 @Component
 class IgniteCacheConcurency {
     @Autowired
     lateinit var ignite: Ignite
-
-
-    //    fun getCache(name: String, myfunc: (IgniteCache<String, Any>) -> Unit) {
-//        println("-----start -concurrency ${Thread.currentThread().name}------------")
-////        var cache = ignite.getOrCreateCache<String, Any>(name)
-//
-//
-//        //    println("------concurrency ${Thread.currentThread().name}---cache---------")
-//
-//        var mylock = getLock(name)
-//
-//        try {
-//            println("------concurrency ${Thread.currentThread().name}--------lock1----")
-//
-//            val cache = ignite.getOrCreateCache<String, Any>(name)
-//
-//            mylock.lock()
-//
-//            println("------concurrency ${Thread.currentThread().name}--------lock2----")
-//            println("------concurrency ${Thread.currentThread().name}--------lock2--size ${cache.size()}--")
-//
-//            if (cache.size() > 0) {
-//                println("------concurrency ${Thread.currentThread().name}---CACHE SIZE > 0---------")
-//
-////                mylock.unlock()
-//                println("------concurrency ${Thread.currentThread().name}---CACHE SIZE > 0-- UNLOCK-------")
-//
-//                return cache
-//            }
-//            println("------concurrency ${Thread.currentThread().name}---process---------")
-//
-//            myfunc(cache)
-////            mylock.unlock()
-//            println("------concurrency ${Thread.currentThread().name}---process--done-------")
-//
-//            return cache
-//        } finally {
-//            println("------concurrency ${Thread.currentThread().name}---FINALLY---------")
-//
-//            mylock.unlock()
-//        }
-//    }
-//
-//    private fun getLock(name: String): Lock {
-//        val cc = CacheConfiguration<String, Any>("mylock-$name")
-//        cc.setAtomicityMode(CacheAtomicityMode.TRANSACTIONAL)
-//        val cache = ignite.getOrCreateCache<String, Any>(cc)
-//        return cache.lock("mylock")
-////
-////    }
-//    fun process(obj: JsonObject, lockcache: IgniteCache<String, Int>, myfunc: (JsonObject) -> Unit) {
-//        println("-----start -concurrency ${Thread.currentThread().name}------------")
-////        var cache = ignite.getOrCreateCache<String, Any>(name)
-//
-//
-//        //    println("------concurrency ${Thread.currentThread().name}---cache---------")
-//
-//        var mylock = lockcache.lock("mylock")
-//
-//        try {
-//            println("------concurrency ${Thread.currentThread().name}--------lock1----")
-//
-////            val cache = ignite.getOrCreateCache<String, Any>(name)
-//
-//            mylock.lock()
-//
-//            println("------concurrency ${Thread.currentThread().name}--------lock2----")
-//            println("------concurrency ${Thread.currentThread().name}--------lock2--size ${lockcache.size()}--")
-//
-//            if (lockcache.get("run") > 0) {
-//                println("------concurrency ${Thread.currentThread().name}---CACHE SIZE > 0---------")
-//
-////                mylock.unlock()
-//                println("------concurrency ${Thread.currentThread().name}---CACHE SIZE > 0-- UNLOCK-------")
-//                return
-//                //return cache
-//            }
-//            println("------concurrency ${Thread.currentThread().name}---process---------")
-//            lockcache.put("run", 1)
-//            myfunc(obj)
-////            mylock.unlock()
-//            println("------concurrency ${Thread.currentThread().name}---process--done-------")
-//
-//            //return cache
-//            return
-//        } finally {
-//            println("------concurrency ${Thread.currentThread().name}---FINALLY---------")
-//            mylock.unlock()
-//        }
-//    }
-//
-//    fun getLock(name: String): IgniteCache<String, Int> {
-//        val cc = CacheConfiguration<String, Int>("$name")
-//        cc.setAtomicityMode(CacheAtomicityMode.TRANSACTIONAL)
-//        var cache = ignite.getOrCreateCache<String, Int>(cc)
-//        cache.put("run", 0)
-//        return cache
-//
-//
-//    }
-
-    //    }
-    fun process(name: String, obj: JsonObject, myfunc: (JsonObject) -> Unit) {
+   /*
+    this is to prevent 2 thread simultaneous calling the process method , both thread in here
+     */
+    fun process(name: String, obj: JsonObject, callback: (JsonObject) -> Unit) {
         println("-----start -concurrency ${Thread.currentThread().name}------------")
 //        var cache = ignite.getOrCreateCache<String, Any>(name)
 
@@ -135,22 +39,14 @@ class IgniteCacheConcurency {
             println("------concurrency ${Thread.currentThread().name}--------lock2----")
             println("------concurrency ${Thread.currentThread().name}--------lock2--size ${lockcache.size()}--")
 
-            if (lockcache.get("run") > 0) {
-                println("------concurrency ${Thread.currentThread().name}---CACHE SIZE > 0---------")
-
-//                mylock.unlock()
-                println("------concurrency ${Thread.currentThread().name}---CACHE SIZE > 0-- UNLOCK-------")
+            if (lockcache.get("run") > 0) {//detect 2nd thread , count >1 if 1st thread run , and 2nd thread come in just exit
+                lockcache.put("run", 0)
                 return
-                //return cache
-            }
-            println("------concurrency ${Thread.currentThread().name}---process---------")
-            lockcache.put("run", 1)
-            myfunc(obj)
-//            mylock.unlock()
-            println("------concurrency ${Thread.currentThread().name}---process--done-------")
 
-            //return cache
-            return
+            }else{
+                lockcache.put("run", 1)
+                callback(obj)
+            }
         } finally {
             println("------concurrency ${Thread.currentThread().name}---FINALLY---------")
             mylock.unlock()
