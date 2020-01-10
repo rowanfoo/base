f0ab0832dd9ca23b1726210f621675ec0dbd0d7c
rowanfoo <rowanfoo@gmail.com>
2020-01-10 21:57:16 +0800

    cleanup

--- algodata/src/main/kotlin/com/dhamma/ignitedata/service/MaIgniteService.kt
+++ algodata/src/main/kotlin/com/dhamma/ignitedata/service/MaIgniteService.kt
@@ -14,10 +14,8 @@ class MaIgniteService {
 
     @Autowired
     lateinit var ignitecache: IgniteRepo<CoreData>
-
     @Autowired
     lateinit var stocklist: List<String>
-
     @Autowired
     lateinit var ignite: Ignite
 
@@ -47,39 +45,24 @@ class MaIgniteService {
             return newcache.get(code)
         }
         return cache.get(code)
-
-
     }
 
     fun process(a: JsonObject) {
 
         var no: Int = a.get("ma").asInt
         var mode: String = a.get("mode").asString
-
-        println("--------load--------MA$no--")
-
-
         var cache = ignite.getOrCreateCache<String, Double>("MA$no:$mode")
 
         if (cache.size() == 0) {
             stocklist.parallelStream().forEach {
-                // var series = ignitecache.get("$it")
-                println("--------load----CODE-------------$it")
                 var series = ignitecache.values(" where code=?  order by date desc  LIMIT ? ", arrayOf(it, "$no"))
-
-                println("--------load----SIZE-------------${series.size}")
-
                 var num = Calc().mocingaverage(no, series, mode)
-
-                println("--------AVG--------$it------------$num")
                 cache.put("$it", num)
 
             }
 
         }
 
-
     }
-
-
 }
+
