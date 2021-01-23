115dd6f532d919b482b5a1d997342b997a57fc1e
rowanfoo <rowanfoo@gmail.com>
2021-01-23 09:01:49 +0800

    add import

--- algodata/src/main/kotlin/com/dhamma/ignitedata/utility/AOP_ApiController.kt
+++ algodata/src/main/kotlin/com/dhamma/ignitedata/utility/AOP_ApiController.kt
@@ -2,7 +2,9 @@ package com.dhamma.ignitedata.utility
 
 import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.annotation.AfterThrowing
+import org.aspectj.lang.annotation.Aspect
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Configuration
 
 
 @Aspect
