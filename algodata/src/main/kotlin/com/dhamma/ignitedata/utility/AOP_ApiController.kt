406edeb890a80757774c8d24adf9bd7bd71f8997
rowanfoo <rowanfoo@gmail.com>
2021-01-23 09:01:19 +0800

    add email error comming from persistent

--- algodata/src/main/kotlin/com/dhamma/ignitedata/utility/AOP_ApiController.kt
+++ algodata/src/main/kotlin/com/dhamma/ignitedata/utility/AOP_ApiController.kt
@@ -2,9 +2,7 @@ package com.dhamma.ignitedata.utility
 
 import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.annotation.AfterThrowing
-import org.aspectj.lang.annotation.Aspect
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Configuration
 
 
 @Aspect
@@ -29,6 +27,25 @@ class AOP_ApiController {
 //        }
         println("ERRRRRRRRRRRRRR---e---------- -> $s")
         email.sendSimpleMessage("joinPoint :${joinPoint.toString()}  \n  $s")
+    }
+
+
+    @AfterThrowing(pointcut = "(execution(*  com.dhamma.ignitedata.service.*.*(..)))", throwing = "e")
+    fun logServiceError(joinPoint: JoinPoint, e: Exception) {
+        println("ERRRRRRRRRRRRRR---SERVICE----1------------ -> $joinPoint")
+        println("ERRRRRRRRRRRRRR----SERVICE-2---------- -> $e")
+        println("ERRRRRRRRRRRRRR----SERVICE-2---------- -> ${e.stackTrace}")
+
+//        println("ERRRRRRRRRRRRRR---2---------- -> ${
+//            e.stackTrace.}")
+
+        var s = e.getStackTrace().toList().drop(0).map(java.lang.StackTraceElement::toString).reduce { s1, s2 -> s1.toString() + "\n" + s2 }
+//        var s = "joinPoint :${joinPoint.toString()}  \n";
+//        for (elem in e.stackTrace) {
+//            s = s + elem.toString() + "\n"
+//        }
+        println("ERRRRRRRRRRRRRR----SERVICE-e---------- -> $s")
+        email.sendSimpleMessage("joinPoint-----SERVICE--- :${joinPoint.toString()}  \n  $s")
     }
 
 }
